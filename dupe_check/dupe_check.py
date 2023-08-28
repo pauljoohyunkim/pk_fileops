22e08217af2bc9b248fefdd42ac7ccff03834463
pauljoohyunkim <80860655+pauljoohyunkim@users.noreply.github.com>
2023-08-29 02:31:18 +0900

    Calling main when using it from interpreter

--- dupe_check/dupe_check.py
+++ dupe_check/dupe_check.py
@@ -1,9 +1,18 @@
+#!/bin/python
+# Debug with python -m dupe_check.dupe_check
 import os
 from sys import argv, exit
 from hasher.hasher import sha256_hexdigest
 
 def main():
     argc = len(argv)
+
     if argc == 1:
-        print("Usage: {argv[1]} [Path] ...")
+        print(f"Usage: {argv[0]} [Path] ...")
         exit(1)
+    
+    # Removing duplicates from the given paths
+    dirlists = list(set(argv[1:]))
+
+if __name__ == "__main__":
+    main()
