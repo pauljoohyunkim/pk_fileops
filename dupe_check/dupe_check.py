5c9275f312d666d53d2be7a16fb3753154aeb780
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-08-29 22:28:14 +0900

    Outputting to file

--- dupe_check/dupe_check.py
+++ dupe_check/dupe_check.py
@@ -8,69 +8,88 @@ from tqdm import tqdm
 from gc import collect
 
 def main():
-    argc = len(argv)
-    # Argument parsing
-    parser = ArgumentParser(description="Checks for duplicate files by comparing hashes")
-    parser.add_argument("directory", nargs="*")
-    parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
-    parser.add_argument("-a", "--absolute", help="Output as absolute paths", action="store_true")
-    args = parser.parse_args()
+    try:
+        argc = len(argv)
+        # Argument parsing
+        parser = ArgumentParser(description="Checks for duplicate files by comparing hashes")
+        parser.add_argument("directory", nargs="*")
+        parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
+        parser.add_argument("-a", "--absolute", help="Output as absolute paths", action="store_true")
+        parser.add_argument("-o", "--output", help="Output the result to the specified file.", nargs=1)
+        args = parser.parse_args()
 
-    verbose = False
-    if args.verbose:
-        verbose = True
-        print("Verbose output enabled.")
+        verbose = False
+        if args.verbose:
+            verbose = True
+            print("Verbose output enabled.")
 
-    absolute = False
-    if args.absolute:
-        absolute = True
+        absolute = False
+        if args.absolute:
+            absolute = True
+            if verbose:
+                print("Absolute paths enabled")
+        
+        outputfile = None
+        if args.output:
+            outputfile = args.output[0]
+            if verbose:
+                print(f"Outputting the result to {outputfile}")
+
+        progresser = tqdm if verbose else (lambda x : x)
+        pather = (lambda x : os.path.abspath(x)) if absolute else (lambda x : x)
+
+        if argc == 1 or args.directory == []:
+            parser.print_usage()
+            exit(1)
+        
+        dirlist = list(set(args.directory))
+
+        # Walk and get a list of files
         if verbose:
-            print("Absolute paths enabled")
+            print("Walking through the directories...")
+        file_digest_pair_list = []
+        for dirname in dirlist:
+            for root, dirs, files in progresser(os.walk(dirname)):
+                for file in files:
+                    filename = pather(os.path.join(root, file))
+                    sha256digest = sha256_hexdigest(filename)
+                    # Check if there was error.
+                    if not sha256digest:
+                        continue
+                    file_digest_pair_list.append((filename, sha256digest))
+        
+        del dirlist
+        collect()
 
-    progresser = tqdm if verbose else (lambda x : x)
-    pather = (lambda x : os.path.abspath(x)) if absolute else (lambda x : x)
+        if verbose:
+            print("Processing the file hash list.")
+        # Process the list such that it creates a dictionary of the form hexdigest:[file path]
+        hash_to_filename = dict()
+        for filename, hexdigest in progresser(file_digest_pair_list):
+            if hexdigest in hash_to_filename.keys():
+                hash_to_filename[hexdigest].append(filename)
+            else:
+                hash_to_filename[hexdigest] = [filename]
 
-    if argc == 1 or args.directory == []:
-        parser.print_usage()
-        exit(1)
-    
-    dirlist = list(set(args.directory))
-
-    # Walk and get a list of files
-    if verbose:
-        print("Walking through the directories...")
-    file_digest_pair_list = []
-    for dirname in dirlist:
-        for root, dirs, files in progresser(os.walk(dirname)):
-            for file in files:
-                filename = pather(os.path.join(root, file))
-                sha256digest = sha256_hexdigest(filename)
-                # Check if there was error.
-                if not sha256digest:
-                    continue
-                file_digest_pair_list.append((filename, sha256digest))
-    
-    del dirlist
-    collect()
-
-    if verbose:
-        print("Processing the file hash list.")
-    # Process the list such that it creates a dictionary of the form hexdigest:[file path]
-    hash_to_filename = dict()
-    for filename, hexdigest in progresser(file_digest_pair_list):
-        if hexdigest in hash_to_filename.keys():
-            hash_to_filename[hexdigest].append(filename)
+        del file_digest_pair_list
+        collect()
+        
+        # Show the entries that are duplicates only.
+        if outputfile:
+            with open(outputfile, "a") as file:
+                for key, value in hash_to_filename.items():
+                    if len(value) == 1:
+                        continue
+                    line = ';'.join(value)
+                    print(line)
+                    file.write(line + "\n")
         else:
-            hash_to_filename[hexdigest] = [filename]
-
-    del file_digest_pair_list
-    collect()
-    
-    # Show the entries that are duplicates only.
-    for key, value in hash_to_filename.items():
-        if len(value) == 1:
-            continue
-        print(';'.join(value))
+            for key, value in hash_to_filename.items():
+                if len(value) == 1:
+                    continue
+                print(';'.join(value))
+    except KeyboardInterrupt:
+        exit(2)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pk_fileops',
-    version='0.2',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[
         line.strip() for line in open('requirements.txt')
