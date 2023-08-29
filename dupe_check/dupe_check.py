34905c26a159d9e7eb1038fc32e0ce86e4b96cb0
pauljoohyunkim <80860655+pauljoohyunkim@users.noreply.github.com>
2023-08-29 20:09:46 +0900

    argument parsing basic

--- dupe_check/dupe_check.py
+++ dupe_check/dupe_check.py
@@ -1,6 +1,7 @@
 #!/bin/python
 # Debug with python -m dupe_check.dupe_check
 import os
+from argparse import ArgumentParser
 from sys import argv, exit
 from hasher.hasher import sha256_hexdigest
 from tqdm import tqdm
@@ -8,19 +9,31 @@ from gc import collect
 
 def main():
     argc = len(argv)
+    # Argument parsing
+    parser = ArgumentParser(description="Checks for duplicate files by comparing hashes")
+    parser.add_argument("directory", nargs="*")
+    parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
+    args = parser.parse_args()
 
-    if argc == 1:
-        print(f"Usage: {argv[0]} [Path] ...")
+    verbose = False
+    if args.verbose:
+        verbose = True
+        print("Verbose output enabled.")
+
+    progresser = tqdm if verbose else lambda x : x
+
+    if argc == 1 or args.directory == []:
+        parser.print_usage()
         exit(1)
     
-    # Removing duplicates from the given paths
-    dirlist = list(set(argv[1:]))
+    dirlist = list(set(args.directory))
 
     # Walk and get a list of files
-    print("Walking through the directories...")
+    if verbose:
+        print("Walking through the directories...")
     file_digest_pair_list = []
     for dirname in dirlist:
-        for root, dirs, files in tqdm(os.walk(dirname)):
+        for root, dirs, files in progresser(os.walk(dirname)):
             for file in files:
                 filename = os.path.join(root, file)
                 sha256digest = sha256_hexdigest(filename)
@@ -32,10 +45,11 @@ def main():
     del dirlist
     collect()
 
-    print("Processing the file hash list.")
+    if verbose:
+        print("Processing the file hash list.")
     # Process the list such that it creates a dictionary of the form hexdigest:[file path]
     hash_to_filename = dict()
-    for filename, hexdigest in tqdm(file_digest_pair_list):
+    for filename, hexdigest in progresser(file_digest_pair_list):
         if hexdigest in hash_to_filename.keys():
             hash_to_filename[hexdigest].append(filename)
         else:
